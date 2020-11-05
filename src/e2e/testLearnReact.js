1a7d3e351a8a98245e2e545afc401ea4d81c4c02
Brian Ballsun-Stanton <brian.ballsun-stanton@mq.edu.au>
2020-11-05 17:16:01 +1100

    maybe?

--- src/e2e/testLearnReact.js
+++ src/e2e/testLearnReact.js
@@ -12,23 +12,28 @@ const desiredCaps = {
     // https://www.browserstack.com/docs/app-automate/appium/upload-app-define-custom-id
     'app': 'ElaborationApp',
 
-
     // Specify device and os_version for testing
     'device': 'Google Pixel 3',
     'os_version': '9.0',
-
+    // 'browserstack.appium_version': '1.6.5',
+    // 'appium_version': '1.6.5',
     // Set other BrowserStack capabilities
     // You need to invoke the browserstack/github-actions/setup-env@master GitHub Action also in the job where test scripts will run because this Action sets up the environment variables BROWSERSTACK_USERNAME, BROWSERSTACK_ACCESS_KEY, BROWSERSTACK_BUILD_NAME and BROWSERSTACK_PROJECT_NAME, which are to be used in your test scripts
 
-    // 'project': process.env.BROWSERSTACK_PROJECT_NAME,
-    // 'build': process.env.BROWSERSTACK_BUILD_NAME,
-    'name': 'bbs_first_test'
+    'project': 'Elaboration Github Workflow',
+    'build': 'Node Android BBS',
+    'name': 'bbs_first_test',
+    'debug': true
 };
+
+console.log(desiredCaps['browserstack.user'])
 
 // Initialize the remote Webdriver using BrowserStack remote URL
 // and desired capabilities defined above
 const driver = wd.promiseRemote("http://hub-cloud.browserstack.com/wd/hub");
 
+driver.setImplicitWaitTimeout(5000);
+// http://appium.io/docs/en/commands/session/timeouts/implicit-wait/
 driver
     .init(desiredCaps)
     .then(function () {
@@ -37,19 +42,12 @@ driver
         && asserters.isEnabled, 30000);
     })
     .then(function (learnReact) {
-        return learnReact.click();
+        return assert.notStrictEqual(learnReact.getAttribute("className"), 'App-link')
     })
-    .then(function () {
-        return driver.element('xpath', ("//*[@text='Chrome']"));
+    .then(function (learnReact) {
+        return assert.notStrictEqual('bad', 'App-link')
     })
-    .then(function (chrome) {
-        chrome.click();
-        return chrome.click();
-    })
-    .then(function () {
-        driver.setImplicitWaitTimeout(10000);
-        return assert.notStrictEqual(driver.url(), 'reactjs.org');
-    })
+    
     .fin(function () {
         return driver.quit();
     })
